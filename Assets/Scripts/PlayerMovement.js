a8eea82359498b8aa2b35095ccc1e2b5a44c605e
Zachary McHenry <zmchenry@gmail.com>
2014-02-03 06:08:49 -0500

    Added recoil and aiming to the gun. There may need to be some slight adjustments to the arms again. Just positioning relative to the body. Also the rotation of the gun may need some adjusting too. Load the first scene to see the changes. I did however keep Ken's prefab in the project until we feel safe deleting it.

---
+++ Assets/Scripts/PlayerMovement.js
@@ -0,0 +1,50 @@
+﻿var walkAcceleration       : float = 5;
+var walkDeacceleration     : float = 5;
+var cameraObject 	       : GameObject;
+var maxWalkSpeed	       : float = 20;
+var maxSlope               : float = 60;
+var jumpVelocity           : float = 20;
+var walkAccelAirRatio      : float = 0.1;
+@HideInInspector
+var walkDeaccelerationVolx : float;
+@HideInInspector
+var walkDeaccelerationVolz : float;
+@HideInInspector
+var grounded         	   : boolean = false;
+@HideInInspector
+var horizontalMovement     : Vector2;
+function Update () {
+	horizontalMovement = Vector2(rigidbody.velocity.x, rigidbody.velocity.z);
+	if(horizontalMovement.magnitude > maxWalkSpeed) {
+		horizontalMovement = horizontalMovement.normalized;
+		horizontalMovement *= maxWalkSpeed;
+	}
+	rigidbody.velocity.x = horizontalMovement.x;
+	rigidbody.velocity.z = horizontalMovement.y;
+	
+	if(grounded) {
+		rigidbody.velocity.x = Mathf.SmoothDamp(rigidbody.velocity.x, 0, walkDeaccelerationVolx, walkDeacceleration);
+		rigidbody.velocity.z = Mathf.SmoothDamp(rigidbody.velocity.z, 0, walkDeaccelerationVolz, walkDeacceleration);
+	}
+	
+	if(grounded) {			
+		rigidbody.AddRelativeForce(Time.deltaTime * walkAcceleration * Input.GetAxis("Horizontal"), 0, Time.deltaTime * walkAcceleration * Input.GetAxis("Vertical"));
+	} else {
+		rigidbody.AddRelativeForce(Time.deltaTime * walkAccelAirRatio * walkAcceleration * Input.GetAxis("Horizontal"), 0, Time.deltaTime * walkAcceleration * walkAccelAirRatio * Input.GetAxis("Vertical"));
+	}
+	
+	if(Input.GetButtonDown("Jump") && grounded)
+		rigidbody.AddForce(0, jumpVelocity, 0);
+}
+// Jump when feet are planted.
+function OnCollisionStay (collision : Collision) {
+	for( var contact: ContactPoint in collision.contacts) {
+		if(Vector3.Angle(contact.normal, Vector3.up) < maxSlope) {
+			grounded = true;
+		}	
+	}
+}
+// When notify that you are in the air.
+function OnCollisionExit() {
+	grounded = false;
+}
---
+++ Assets/Scripts/ShootingScript.js
@@ -0,0 +1,36 @@
+﻿var recoilMod : Transform;
+var weapon : GameObject;
+var maxRecoil_x : float = -20;
+var recoilSpeed : float = 10;
+var recoil : float = 0.0;
+
+function Update() {
+    if(Input.GetMouseButtonDown(0))
+    {
+        //every time you fire a bullet, add to the recoil.. of course you can probably set a max recoil etc..
+        recoil+= 0.1;
+    }
+    recoiling();
+}
+function recoiling() {
+    if(recoil > 0)
+    {
+    	// Get the max rotation on the z axis
+        var maxRecoil = Quaternion.Euler (0, 0, maxRecoil_x);
+        // Dampen towards the target rotation
+        recoilMod.rotation = Quaternion.Slerp(recoilMod.rotation, maxRecoil, Time.deltaTime * recoilSpeed);
+        // commit change
+        weapon.transform.localEulerAngles.z = recoilMod.localEulerAngles.z;
+        recoil -= Time.deltaTime;
+    }
+    else
+    {
+    // return to norm
+        recoil = 0;
+        var minRecoil = Quaternion.Euler (0, 0, 0);
+        // Dampen towards the target rotation
+        recoilMod.rotation = Quaternion.Slerp(recoilMod.rotation, minRecoil,Time.deltaTime * recoilSpeed / 2);
+        weapon.transform.localEulerAngles.z = recoilMod.localEulerAngles.z;
+    }
+
+}
